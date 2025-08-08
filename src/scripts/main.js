53beebe1cc4a384031141541f24d0e7bc7b81259
Ivan <kovalevivan420@gmail.com>
2025-08-08 19:19:46 +0200

    new solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const topPosition = (wall.clientHeight - spider.offsetHeight) / 2;
+const leftPosition = (wall.clientWidth - spider.offsetWidth) / 2;
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
